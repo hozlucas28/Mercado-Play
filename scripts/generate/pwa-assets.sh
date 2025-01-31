60c96e25838d4b358335e2234b0c6117fe52e428
hozlucas28 <hozlucas28@gmail.com>
2025-01-31 02:59:43 -0300

    chore: add new prod and dev dependencies, and a script
    - Added dev dependencies to generate PWA assets with `generate:pwa-assets` (npm script).

---
+++ scripts/generate/pwa-assets.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Generate PWA assets
+pwa-asset-generator \
+    ./public/favicon-512x512.png \
+    ./public/icons/ \
+    --opaque false \
+    --padding '0px' \
+    --scrape \
+    --manifest ./public/manifest.json \
+    --path '/' \
+    --path-override 'icons' \
+    --type png \
+    --mstile \
+    --maskable \
+    --log &&
+
+# Optimize PNG images
+pngquant \
+    --force \
+    --skip-if-larger \
+    --ext .png \
+    --speed 1 \
+    --strip \
+    32 \
+    -- \
+    ./public/icons/*.png
+
