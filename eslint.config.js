6f53bf51d37b9fd36f8c12e04190b53399d10204
hozlucas28 <hozlucas28@gmail.com>
2025-03-08 16:52:18 -0300

    chore: add `eslint-plugin-astro` dev dependency

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,27 @@
 import pluginJs from '@eslint/js'
+import pluginAstro from 'eslint-plugin-astro'
 import pluginReact from 'eslint-plugin-react'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{ files: ['@(src|scripts)/**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-	{ languageOptions: { globals: globals.browser } },
+	{
+		files: ['@(src|scripts)/**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+	},
+	{
+		languageOptions: {
+			globals: globals.browser,
+		},
+	},
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
+	...pluginAstro.configs.recommended,
 	{
 		...pluginReact.configs.flat.recommended,
 		rules: {
 			...pluginReact.configs.flat.recommended.rules,
+			'react/react-in-jsx-scope': 'off',
 			'react/jsx-sort-props': [
 				'warn',
 				{
@@ -22,7 +31,12 @@ export default [
 					shorthandLast: true,
 				},
 			],
-			'react/react-in-jsx-scope': 'off',
+		},
+	},
+	{
+		files: ['@(src|scripts)/**/*.astro'],
+		rules: {
+			'react/no-unknown-property': ['off'],
 		},
 	},
 ]
