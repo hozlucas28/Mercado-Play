69441b42c3fd99bba55540fcf827a4905450005f
hozlucas28 <hozlucas28@gmail.com>
2025-03-26 19:42:58 -0300

    fix: theme provider

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,64 +1,24 @@
-import { DEFAULT_THEME, DEFAULT_THEME_STORAGE_KEY } from '@/constants'
+import { THEME } from '@/constants'
 import type { Theme } from '@/types'
-import { createContext, useContext, useEffect, useState } from 'react'
 
-type ThemeProviderState = {
-	theme: Theme
-	setTheme: (theme: Theme) => void
-}
+import { useStore } from '@nanostores/react'
+import { useEffect } from 'react'
 
-const initialState: ThemeProviderState = {
-	theme: DEFAULT_THEME,
-	setTheme: () => null,
-}
-
-const ThemeProviderContext = createContext(initialState)
-
-type ThemeProviderProps = {
-	children: React.ReactNode
-	defaultTheme?: Theme
-	storageKey?: string
-}
-
-export function ThemeProvider({
-	children,
-	defaultTheme = DEFAULT_THEME,
-	storageKey = DEFAULT_THEME_STORAGE_KEY,
-	...props
-}: ThemeProviderProps) {
-	const [theme, setTheme] = useState(
-		() => (localStorage.getItem(storageKey) as Theme) || DEFAULT_THEME
-	)
-
-	useEffect(() => {
-		const darkTheme: Theme = 'dark'
-		const lightTheme: Theme = 'light'
-		window.document.documentElement.classList.remove(lightTheme, darkTheme)
-		window.document.documentElement.classList.add(theme)
-	}, [theme])
-
-	const value = {
-		theme,
-		setTheme: (theme: Theme) => {
-			localStorage.setItem(storageKey, theme)
-			setTheme(theme)
-		},
-	}
-
-	return (
-		<ThemeProviderContext.Provider
-			{...props}
-			value={value}
-		>
-			{children}
-		</ThemeProviderContext.Provider>
-	)
-}
+import { themeStore } from './stores/theme'
 
 export const useTheme = () => {
-	const context = useContext(ThemeProviderContext)
-	if (context === undefined)
-		throw new Error('useTheme must be used within a ThemeProvider')
+	const currentTheme = useStore(themeStore)
 
-	return context
+	useEffect(() => {
+		window.document.documentElement.classList.remove(...THEME.values)
+		window.document.documentElement.classList.add(currentTheme)
+	}, [currentTheme])
+
+	return {
+		theme: currentTheme,
+		setTheme: (newTheme: Theme) => {
+			themeStore.set(newTheme)
+			localStorage.setItem(THEME.storageKey, newTheme)
+		},
+	}
 }
