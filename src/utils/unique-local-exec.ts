1f1484fa2d69550068b3968b206e484a7526d66a
hozlucas28 <hozlucas28@gmail.com>
2025-03-26 14:14:32 -0300

    feature: create `unique-local-exec.ts`

---
+++ src/utils/unique-local-exec.ts
@@ -0,0 +1,59 @@
+interface LocalExecs {
+	pending: string[]
+	executed: string[]
+}
+
+interface Options {
+	id: string
+	localStorageID: string
+}
+
+function uniqueLocalExec<T>(
+	callback: (id: string, setExecuted: () => void) => T,
+	options: Options
+): (() => T) | undefined {
+	const { id, localStorageID } = options
+
+	try {
+		const localExecs = Object.assign<LocalExecs, LocalExecs>(
+			{
+				pending: [],
+				executed: [],
+			},
+			JSON.parse(localStorage.getItem(localStorageID) ?? '{}')
+		)
+
+		if (localExecs.executed.includes(id)) return
+
+		const setExecuted = () => {
+			const localExecs = Object.assign<LocalExecs, LocalExecs>(
+				{
+					pending: [],
+					executed: [],
+				},
+				JSON.parse(localStorage.getItem(localStorageID) ?? '{}')
+			)
+
+			for (let i = 0; i < localExecs.pending.length; i++) {
+				if (localExecs.pending[i] === id) {
+					localExecs.pending.splice(i)
+					break
+				}
+			}
+
+			localExecs.executed.push(id)
+			localStorage.setItem(localStorageID, JSON.stringify(localExecs))
+		}
+
+		if (!localExecs.pending.includes(id)) {
+			localExecs.pending.push(id)
+			localStorage.setItem(localStorageID, JSON.stringify(localExecs))
+		}
+
+		return () => callback(id, setExecuted)
+	} catch (error) {
+		return
+	}
+}
+
+export { uniqueLocalExec }
