f8412fa170b01d6c34b9ac6c9878e74035b3fa51
hozlucas28 <hozlucas28@gmail.com>
2025-04-07 05:18:10 -0300

    feature: improve aria

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -6,26 +6,26 @@ import type { ComponentProps } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 function ThemeSwitcher(props: Omit<ComponentProps<typeof Button>, 'children' | 'aria-label' | 'onPress'>) {
-	const [currentTheme, switchTheme] = useSwitchTheme()
+	const [theme, switchTheme] = useSwitchTheme()
 
 	return (
 		<Button
 			size='square-petite'
 			intent='outline'
-			aria-label='Cambiar tema'
+			aria-label={`Cambiar a tema ${theme === 'light' ? 'oscuro' : 'claro'}`}
 			onPress={switchTheme}
 			{...props}
 		>
 			<IconSun
 				className={twMerge(
-					'h-[1.2rem] w-[1.2rem] transition-all',
-					currentTheme === 'dark' ? 'scale-0 -rotate-90 opacity-0' : 'scale-100 rotate-0 opacity-100'
+					'size-[1.2rem] transition-all',
+					theme === 'dark' ? 'scale-0 -rotate-90 opacity-0' : 'scale-100 rotate-0 opacity-100'
 				)}
 			/>
 			<IconMoon
 				className={twMerge(
-					'absolute h-[1.2rem] w-[1.2rem] transition-all',
-					currentTheme === 'dark' ? 'scale-100 rotate-0 opacity-100' : 'scale-0 rotate-90 opacity-0'
+					'absolute size-[1.2rem] transition-all',
+					theme === 'dark' ? 'scale-100 rotate-0 opacity-100' : 'scale-0 rotate-90 opacity-0'
 				)}
 			/>
 		</Button>
